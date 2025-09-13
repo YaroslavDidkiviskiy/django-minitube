0d70a57f97b51a4b62be6c550609d59eeda43a5a
YaroslavDidkivskiy <yaroslavdidkivskiy@gmail.com>
2025-09-13 18:03:42 +0300

    model for video

--- videos/models.py
+++ videos/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Status(models.TextChoices):
+	PENDING = "PENDING", "Pending"
+	PROCESSING = "PROCESSING", "Processing"
+	READY = "READY", "Ready"
+	FAILED = "FAILED", "Failed"
+
+
+class Video(models.Model):
+	title = models.CharField(max_length=120)
+	file_original = models.FileField(upload_to='uploads/')
+	file_converted = models.FileField(upload_to='converted/', null=True, blank=True)
+	status = models.CharField(
+		max_length=120,
+		choices=Status.choices,
+		default="Pending",
+	)
+	views = models.IntegerField(default=0)
+	likes = models.IntegerField(default=0)
+	dislikes = models.IntegerField(default=0)
+	duration = models.IntegerField(default=0)
+	created_at = models.DateTimeField(auto_now_add=True)
+	slug = models.SlugField(max_length=120, unique=True)
+	
+	def __str__(self):
+		return self.title
+	
